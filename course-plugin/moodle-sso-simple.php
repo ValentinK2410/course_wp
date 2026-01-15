ff742d4ca385be36cb33391832e9266d708fa536
Valentink2410 <valentink2410@gmail.com>
2026-01-15 12:31:33 +0400

    Добавлен простейший тестовый скрипт без проверок

---
+++ course-plugin/moodle-sso-simple.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Простейший тестовый скрипт без проверок
+ * Просто выводит alert для проверки загрузки
+ */
+header('Content-Type: application/javascript; charset=utf-8');
+?>
+alert('MOODLE SSO: Скрипт загружен успешно!');
+console.log('MOODLE SSO: Скрипт выполнен в ' + new Date().toLocaleString());
+console.log('MOODLE SSO: Файл moodle-sso-simple.php работает!');
